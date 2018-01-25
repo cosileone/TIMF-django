0d227333f15b4d31096d9a65b6b5f97076c8f2a3
Cosimo Leone <cosi.leone@icloud.com>
2018-01-24 23:41:49 -0500

    change item display in admin

--- items/admin.py
+++ items/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-
-# Register your models here.
 from .models import Item
 
-admin.site.register(Item)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+    ordering = ['id']
+
