39ebd6019fb6957e87b24dc0e24269b6babd25d5
Cosimo Leone <cosi.leone@icloud.com>
2018-01-28 21:33:14 -0500

    introduce ingredients throughtable based off tblDBCitemreagents

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from .managers import RecipeManager
+from .managers import RecipeQuerySet
 
 
 # Create your models here.
@@ -25,12 +25,50 @@ class Recipe(models.Model):
         default=0.00
     )
 
-    crafteditem = models.PositiveIntegerField()
+    crafteditem = models.ForeignKey(
+        'items.Item',
+        related_name='crafting_recipe',
+        verbose_name='Crafting Recipe',
+        db_column='crafteditem',
+        on_delete=models.PROTECT,
+    )
+
+    ingredients = models.ManyToManyField(
+        'items.Item',
+        through='Ingredient',
+        through_fields=('spell', 'reagent'),
+        related_name='recipes',
+    )
 
     expansion = models.PositiveSmallIntegerField()
 
-    objects = RecipeManager()
+    objects = RecipeQuerySet.as_manager()
 
     class Meta:
         db_table = 'tblDBCSpell'
         ordering = ['id']
+
+
+class Ingredient(models.Model):
+    item = models.ForeignKey(
+        'items.Item',
+        on_delete=models.PROTECT,
+        db_column='item',
+        related_name='reagent_for'
+    )
+    skillline = models.PositiveSmallIntegerField()
+    reagent = models.ForeignKey(
+        'items.Item',
+        on_delete=models.PROTECT,
+        db_column='reagent'
+    )
+    quantity = models.DecimalField(max_digits=8, decimal_places=4)
+    spell = models.ForeignKey(
+        'Recipe',
+        on_delete=models.PROTECT,
+        db_column='spell',
+        blank=True, null=True
+    )
+
+    class Meta:
+        db_table = 'tblDBCItemReagents'
