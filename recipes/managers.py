013435a56b8e32885f8047cc8f0ce637bb0d5cbc
Cosimo Leone <cosi.leone@icloud.com>
2018-01-28 21:32:48 -0500

    don't use manager, use queryset as manager instead!

--- recipes/managers.py
+++ recipes/managers.py
@@ -1,9 +1,8 @@
 from django.db import models
-from itertools import chain
 
 
-class RecipeManager(models.Manager):
-    def get_queryset(self):
-        default_results = super(RecipeManager, self).get_queryset().using('default').exclude(skillline__isnull=True)
-        newsstand_results = super(RecipeManager, self).get_queryset().exclude(skillline__isnull=True)
+class RecipeQuerySet(models.QuerySet):
+    def auctionable(self):
+        default_results = self.using('default').exclude(skillline__isnull=True)
+        newsstand_results = self.exclude(skillline__isnull=True)
         return newsstand_results.order_by('id').union(default_results.order_by('id'))
