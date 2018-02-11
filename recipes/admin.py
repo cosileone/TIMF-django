c5a895094731f536c7a5901895a880f979e3131c
Cosimo Leone <cosi.leone@icloud.com>
2018-02-11 00:55:18 -0500

    pretty ingredients admin page

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Recipe, Ingredient
 
@@ -24,7 +26,32 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ['reagent', 'quantity', 'spell', 'item']
+    list_display = ['reagent_link', 'quantity', 'spell_link', 'item_link']
+    list_select_related = (
+        'reagent', 'spell', 'item'
+    )
     list_display_links = None
     search_fields = ['item__name', 'spell__name', 'reagent__name']
     ordering = ['spell']
+
+    def spell_link(self, obj):
+        return mark_safe('<a href="{0}">{1}</a>'.format(
+            reverse('admin:recipes_recipe_change', args=(obj.pk,)),
+            obj.spell
+        ))
+
+    spell_link.short_description = 'Recipe'
+
+    def item_link(self, obj):
+        return mark_safe('<a href="{0}">{1}</a>'.format(
+            reverse('admin:items_item_change', args=(obj.item.pk,)),
+            obj.item
+        ))
+    item_link.short_description = 'Item'
+
+    def reagent_link(self, obj):
+        return mark_safe('<a href="{0}">{1}</a>'.format(
+            reverse('admin:items_item_change', args=(obj.reagent.pk,)),
+            obj.reagent
+        ))
+    reagent_link.short_description = 'Reagent'
