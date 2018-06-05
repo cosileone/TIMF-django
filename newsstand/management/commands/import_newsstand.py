2959656bca6fcd2d62abcd4c2ab3b3eafdc456b5
Cosimo Leone <cosi.leone@icloud.com>
2018-06-05 19:18:34 -0400

    unused import

--- newsstand/management/commands/import_newsstand.py
+++ newsstand/management/commands/import_newsstand.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from newsstand.models import Tbldbcitem, Tblrealm
